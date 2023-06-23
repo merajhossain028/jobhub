b5106488017f0938490019f28aac3990e7ba97cd
Md. Meraj Hossain <meraj@nerddevs.com>
2023-06-23 16:21:18 +0600

    set page controller in onBoard Screen

--- lib/views/ui/onboarding/onboarding_screen.dart
+++ lib/views/ui/onboarding/onboarding_screen.dart
@@ -1,24 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:jobhub/controllers/onboarding_provider.dart';
 import 'package:jobhub/views/ui/onboarding/widgets/page_one.dart';
 import 'package:jobhub/views/ui/onboarding/widgets/page_three.dart';
 import 'package:jobhub/views/ui/onboarding/widgets/page_two.dart';
+import 'package:provider/provider.dart';
 
-class OnBoardingScreen extends StatelessWidget {
+class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  final PageController pageController = PageController();
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          PageView(
-            children: const [
-              PageOne(),
-              PageTwo(),
-              PageThree(),
+      body: Consumer<OnBoardNotifier>(
+        builder: (context, onBoardNotifier, child) {
+          return Stack(
+            children: [
+              PageView(
+                physics: onBoardNotifier.isLastPage
+                    ? const NeverScrollableScrollPhysics()
+                    : const AlwaysScrollableScrollPhysics(),
+                controller: pageController,
+                onPageChanged: (page) {
+                  onBoardNotifier.isLastPage = page == 2;
+                },
+                children: const [
+                  PageOne(),
+                  PageTwo(),
+                  PageThree(),
+                ],
+              ),
             ],
-          ),
-        ],
+          );
+        },
       ),
     );
   }
