cf05189481c692d9a76ba0b241b74b21b80d776a
Md. Meraj Hossain <meraj@nerddevs.com>
2023-06-23 16:09:27 +0600

    making custom button and page three ui

--- lib/views/common/custom_outline_btn.dart
+++ lib/views/common/custom_outline_btn.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:jobhub/views/common/app_style.dart';
+import 'package:jobhub/views/common/reusable_text.dart';
 
 class CustomOutlineBtn extends StatelessWidget {
-  const CustomOutlineBtn({super.key});
+  const CustomOutlineBtn({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.text,
+    required this.onTap,
+    required this.color,
+    this.color2,
+  });
+
+  final double width;
+  final double height;
+  final String text;
+  final Function() onTap;
+  final Color color;
+  final Color? color2;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: color2,
+          border: Border.all(
+            width: 1,
+            color: color,
+          ),
+        ),
+        child: Center(
+          child: ReusableText(
+            text: text,
+            style: appstyle(
+              16,
+              color,
+              FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/views/ui/onboarding/widgets/page_three.dart
+++ lib/views/ui/onboarding/widgets/page_three.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jobhub/constants/app_constants.dart';
+import 'package:jobhub/views/common/app_style.dart';
+import 'package:jobhub/views/common/custom_outline_btn.dart';
+import 'package:jobhub/views/common/height_spacer.dart';
+import 'package:jobhub/views/common/reusable_text.dart';
 
 class PageThree extends StatelessWidget {
   const PageThree({super.key});
@@ -9,7 +14,74 @@ class PageThree extends StatelessWidget {
     return Container(
       width: width,
       height: height,
-      color: Color(kDarkBlue.value),
+      color: Color(kLightBlue.value),
+      child: Column(
+        children: [
+          Image.asset('assets/images/page3.png'),
+          const HeightSpacer(size: 20),
+          ReusableText(
+            text: "Welcome to Job Hunt",
+            style: appstyle(
+              30,
+              Color(kLight.value),
+              FontWeight.w600,
+            ),
+          ),
+          const HeightSpacer(size: 15),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.w),
+            child: Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc ut dignissim. ",
+              style: appstyle(
+                14,
+                Color(kLight.value),
+                FontWeight.normal,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const HeightSpacer(size: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CustomOutlineBtn(
+                onTap: () {},
+                text: 'Login',
+                width: width * 0.4,
+                height: height * 0.06,
+                color: Color(kLight.value),
+              ),
+              GestureDetector(
+                onTap: null,
+                child: Container(
+                  width: width * 0.4,
+                  height: height * 0.06,
+                  color: Color(kLight.value),
+                  child: Center(
+                    child: ReusableText(
+                      text: "Sign Up",
+                      style: appstyle(
+                        16,
+                        Color(kLightBlue.value),
+                        FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const HeightSpacer(size: 30),
+          ReusableText(
+            text: "Continue as gust",
+            style: appstyle(
+              16,
+              Color(kLight.value),
+              FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
