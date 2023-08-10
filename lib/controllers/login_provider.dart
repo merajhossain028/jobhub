4b737f363db1709b05edee3964b7f1e799209526
Md. Meraj Hossain <meraj@nerddevs.com>
2023-08-10 15:10:03 +0600

    feat: added show password provider

--- lib/controllers/login_provider.dart
+++ lib/controllers/login_provider.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-
 class LoginNotifier extends ChangeNotifier {
+  bool _obsecureText = true;
 
+  bool get obsecureText => _obsecureText;
+
+  set obsecureText(bool newState) {
+    _obsecureText = newState;
+    notifyListeners();
+  }
 }
